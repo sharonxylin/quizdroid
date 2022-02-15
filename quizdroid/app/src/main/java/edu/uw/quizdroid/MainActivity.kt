db0949bbb017a942a926189664b8914cd67d0d0c
sharonxylin <sl742041@gmail.com>
2022-02-15 14:30:43 -0800

    some progress

--- quizdroid/app/src/main/java/edu/uw/quizdroid/MainActivity.kt
+++ quizdroid/app/src/main/java/edu/uw/quizdroid/MainActivity.kt
@@ -4,8 +4,42 @@ import android.content.Intent
 import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
 import android.util.Log
 import android.widget.Button
+import java.io.BufferedReader
+import java.io.File
+import java.io.InputStream
+import java.io.InputStreamReader
+import java.net.HttpURLConnection
+import java.net.URL
+import kotlin.concurrent.thread
+
+/*
+
+The application should create some background operation (Thread, AlarmManager or Service)
+that will (eventually) attempt to download a JSON file containing the questions from the
+server every "N" minutes/hours. For now, pop a Toast message displaying the URL that will
+eventually be hit. Make sure this URL is what's defined in the Preferences.
+
+The background operation should now attempt to download the JSON file specified in the
+Preferences URL. Save this data to a local file as questions.json. Make sure that this file
+always remains in a good state--if the download fails or is interrupted, the previous file
+should remain in place.
+
+If I am currently offline (in Airplane mode or in a no-bars area) the application should
+display a message telling me I have no access to the Internet. If I am in airplane mode,
+it should ask if I want to turn airplane mode off and take me to the Settings activity to
+do so. If I simply have no signal, it should just punt gracefully with a nice error message.
+
+If the download of the questions fails, the application should tell me and ask if I want to
+retry or quit the application and try again later.
+
+
+*
+* */
+
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,9 +52,37 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val app = this.application as QuizApp
         val prefs: SharedPreferences = getSharedPreferences("settings", MODE_PRIVATE)
-        // prefs.edit().clear().commit()
+        var temp = ""
+
+        //prefs.edit().clear().commit()
         if(prefs.contains("application.firstRun")){
-            //update configuration settings with prefURL and prefMinutes
+            Log.i("MAIN", prefs.getInt("prefMinutes", 0)!!.toString())
+            //write to local file
+            val t =  thread {
+                val server = URL(prefs.getString("prefURL", "")!!)
+                val client: HttpURLConnection = server.openConnection() as HttpURLConnection
+                client.requestMethod = "GET"
+
+
+
+
+                val outputFilePath = applicationContext.filesDir.toString() + "/questions.json"
+
+                Log.i("here", outputFilePath)
+
+                client.inputStream.copyTo(File(outputFilePath).outputStream())
+
+                val result = BufferedReader(InputStreamReader(client.inputStream))
+                var inputLine: String?
+                /*while(result.readLine().also{inputLine=it}!=null)
+                    // temp += inputLine!!.toString()
+                    //temp.plus(inputLine!!.toString())
+                    Log.i("here", inputLine!!.toString())
+                    // File("data/questions.json").outputStream().use{}*/
+                result.close()
+
+                Log.i("here", File(outputFilePath).reader().toString())
+            }
         }else{
             val intent = Intent(this, Preferences::class.java)
             startActivity(intent)
